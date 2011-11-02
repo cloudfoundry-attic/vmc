10626d35ab8c5551811018ec26687af5036054b7
Patrick Bozeman <pbozeman@vmware.com>
2011-11-02 11:46:03 -0700

    bump version to 0.3.13
    Change-Id: I25c5459d5e4998790bda66efa873c3fc8711264e

--- lib/cli/version.rb
+++ lib/cli/version.rb
@@ -2,6 +2,6 @@ module VMC
   module Cli
     # This version number is used as the RubyGem release version.
     # The internal VMC version number is VMC::VERSION.
-    VERSION = '0.3.13.beta.1'
+    VERSION = '0.3.13'
   end
 end
