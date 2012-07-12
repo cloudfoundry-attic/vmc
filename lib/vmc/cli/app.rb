eee788d9fd4258d0a24b3c3619f7266101934bb8
Alex Suraci <suraci.alex@gmail.com>
2012-07-12 16:40:39 -0700

    move app helpers from CLI to App
    Change-Id: If789934f1c7e679b4c5a06b72007cc2397e2dc0f

--- lib/vmc/cli/app.rb
+++ lib/vmc/cli/app.rb
@@ -922,5 +922,49 @@ module VMC
         end
       end
     end
+
+    def usage(used, limit)
+      "#{b(human_size(used))} of #{b(human_size(limit, 0))}"
+    end
+
+    def percentage(num, low = 50, mid = 70)
+      color =
+        if num <= low
+          :good
+        elsif num <= mid
+          :warning
+        else
+          :bad
+        end
+
+      c(format("%.1f\%", num), color)
+    end
+
+    def megabytes(str)
+      if str =~ /T$/i
+        str.to_i * 1024 * 1024
+      elsif str =~ /G$/i
+        str.to_i * 1024
+      elsif str =~ /M$/i
+        str.to_i
+      elsif str =~ /K$/i
+        str.to_i / 1024
+      else # assume megabytes
+        str.to_i
+      end
+    end
+
+    def human_size(num, precision = 1)
+      sizes = ["G", "M", "K"]
+      sizes.each.with_index do |suf, i|
+        pow = sizes.size - i
+        unit = 1024 ** pow
+        if num >= unit
+          return format("%.#{precision}f%s", num / unit, suf)
+        end
+      end
+
+      format("%.#{precision}fB", num)
+    end
   end
 end
