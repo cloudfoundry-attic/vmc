3f1a30a1baae4b726549b6835950fd80600a06e6
Alex Suraci <suraci.alex@gmail.com>
2012-08-15 12:12:42 -0700

    add 'app' command specs
    Change-Id: Ia9226b79a0b4fac7c1a11583dede94d0e477e067

---
+++ spec/app/app_spec.rb
@@ -0,0 +1,19 @@
+require "./helpers"
+
+describe "App#app" do
+  it "shows the app name if it exists" do
+    with_random_app do |app|
+      running(:app, {}, :app => app.name) do
+        outputs(app.name)
+      end
+    end
+  end
+
+  it "fails if an unknown app is provided" do
+    running(:app, {}, :app => "unknown-app") do
+      raises(VMC::UserError) do |e|
+        e.message.should == "Unknown app 'unknown-app'"
+      end
+    end
+  end
+end
