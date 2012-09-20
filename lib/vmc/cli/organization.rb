50a56fa8583335a11dedefc29e433789106b6a2e
Alex Suraci <suraci.alex@gmail.com>
2012-09-20 14:18:13 -0700

    orgs: add -l/--full, improve default output
    Change-Id: I160e3366ef62538ea4618f4a44440ff44c52b8fd

--- lib/vmc/cli/organization.rb
+++ lib/vmc/cli/organization.rb
@@ -53,16 +53,31 @@ module VMC
 
     desc "List available organizations"
     group :organizations
+    input :one_line, :alias => "-l", :type => :boolean, :default => false,
+      :desc => "Single-line tabular format"
+    input :full, :type => :boolean, :default => false,
+      :desc => "Show full information for apps, service instances, etc."
     def orgs
       orgs =
         with_progress("Getting organizations") do
-          client.organizations
+          client.organizations(1)
         end
 
       line unless quiet?
 
-      orgs.each do |o|
-        line c(o.name, :name)
+      if input[:one_line]
+        table(
+          %w{name spaces domains},
+          orgs.collect { |o|
+            [ c(o.name, :name),
+              name_list(o.spaces),
+              name_list(o.domains)
+            ]
+          })
+      else
+        orgs.each do |o|
+          invoke :org, :organization => o, :full => input[:full]
+        end
       end
     end
   end
