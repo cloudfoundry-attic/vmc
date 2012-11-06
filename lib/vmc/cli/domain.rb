69597fbf0bca3387564eebd0777d7e0c6bb0e460
Alex Suraci <suraci.alex@gmail.com>
2012-11-05 20:24:41 -0800

    add-domain: invoke create-domain if not exists
    Change-Id: Id8aac57968067ddb32865913e7057b9f0b4894f8

--- lib/vmc/cli/domain.rb
+++ lib/vmc/cli/domain.rb
@@ -104,23 +104,27 @@ module VMC
       with_progress("Creating domain #{c(name, :name)}") do
         domain.create!
       end
+
+      domain
     end
 
 
     desc "Add a domain to a space"
     group :domains
-    input(:domain, :argument => :optional,
-          :from_given => by_name("domain"),
-          :desc => "Domain to add") {
-      ask "Which domain?", :choices => client.current_organization.domains,
-        :display => proc(&:name)
-    }
+    input :name, :argument => :required,
+      :desc => "Domain to add"
     input :space, :from_given => by_name("space"),
       :default => proc { client.current_space },
       :desc => "Space to add the domain to"
     def add_domain
       space = input[:space]
-      domain = input[:domain]
+      name = input[:name].sub(/^\*\./, "")
+
+      org = space.organization
+
+      domain =
+        org.domains.find { |d| d.name == name } ||
+          invoke(:create_domain, :org => org, :name => name)
 
       with_progress("Adding #{c(domain.name, :name)} to #{c(space.name, :name)}") do
         space.add_domain(domain)
