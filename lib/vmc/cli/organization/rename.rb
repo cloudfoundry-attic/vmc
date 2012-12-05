4a2de119a0c1c2730cafe5a1221777e12c0467bf
Alex Suraci and Tim Labeeuw <suraci.alex@gmail.com>
2012-12-04 17:02:26 -0800

    add rename-org
    Change-Id: I9669212cfcea02c1ba6ebc2742afc808231144b2

---
+++ lib/vmc/cli/organization/rename.rb
@@ -0,0 +1,32 @@
+require "vmc/detect"
+
+require "vmc/cli/organization/base"
+
+module VMC::Organization
+  class Rename < Base
+    desc "Rename an organization"
+    group :organizations
+    input(:organization, :aliases => ["--org", "-o"],
+          :argument => :optional, :desc => "Organization to rename",
+          :from_given => by_name("organization")) {
+      organizations = client.organizations
+      fail "No organizations." if organizations.empty?
+
+      ask("Rename which organization?", :choices => organizations.sort_by(&:name),
+          :display => proc(&:name))
+    }
+    input(:name, :argument => :optional, :desc => "New organization name") {
+      ask("New name")
+    }
+    def rename_org
+      organization = input[:organization]
+      name = input[:name]
+
+      organization.name = name
+
+      with_progress("Renaming to #{c(name, :name)}") do
+        organization.update!
+      end
+    end
+  end
+end
---
+++ spec/vmc/cli/organization/rename_spec.rb
@@ -0,0 +1,113 @@
+require 'spec_helper'
+require "vmc/cli/organization/rename"
+
+describe VMC::Organization::Rename do
+  let(:global_inputs) { { :color => false, :quiet => true } }
+  let(:inputs) { {} }
+  let(:given) { {} }
+  let(:organizations) { FactoryGirl.build_list(:organization, 3) }
+  let(:client) { FactoryGirl.build(:client, :organizations => organizations) }
+  let(:new_name) { "some-new-name" }
+
+  before do
+    any_instance_of(VMC::CLI) do |cli|
+      stub(cli).client { client }
+      stub(cli).precondition { nil }
+    end
+  end
+
+  subject { Mothership.new.invoke(:rename_org, inputs, given, global_inputs) }
+
+  describe 'metadata' do
+    let(:command) { Mothership.commands[:rename_org] }
+
+    describe 'command' do
+      subject { command }
+      its(:description) { should eq "Rename an organization" }
+      it { expect(Mothership::Help.group(:organizations)).to include(subject) }
+    end
+
+    describe 'inputs' do
+      subject { command.inputs }
+
+      it "is not missing any descriptions" do
+        subject.each do |input, attrs|
+          expect(attrs[:description]).to be
+          expect(attrs[:description].strip).to_not be_empty
+        end
+      end
+    end
+
+    describe 'arguments' do
+      subject { command.arguments }
+      it 'has the correct argument order' do
+        should eq([
+          { :type => :optional, :value => nil, :name => :organization },
+          { :type => :optional, :value => nil, :name => :name }
+        ])
+      end
+    end
+  end
+
+  context 'when there are no organizations' do
+    let(:organizations) { [] }
+
+    context 'and an organization is given' do
+      let(:given) { { :organization => "some-invalid-organization" } }
+      it { expect { subject }.to raise_error(VMC::UserError, "Unknown organization 'some-invalid-organization'.") }
+    end
+
+    context 'and an organization is not given' do
+      it { expect { subject }.to raise_error(VMC::UserError, "No organizations.") }
+    end
+  end
+
+  context 'when there are organizations' do
+    let(:renamed_organization) { organizations.first }
+
+    context 'when the defaults are used' do
+      it 'asks for the organization and new name and renames' do
+        mock_ask("Rename which organization?", anything) { renamed_organization }
+        mock_ask("New name") { new_name }
+        mock(renamed_organization).name=(new_name)
+        mock(renamed_organization).update!
+        subject
+      end
+    end
+
+    context 'when no name is provided, but an organization is' do
+      let(:given) { { :organization => renamed_organization.name } }
+
+      it 'asks for the new name and renames' do
+        dont_allow_ask("Rename which organization?", anything)
+        mock_ask("New name") { new_name }
+        mock(renamed_organization).name=(new_name)
+        mock(renamed_organization).update!
+        subject
+      end
+    end
+
+    context 'when an organization is provided and a name' do
+      let(:inputs) { { :organization => renamed_organization, :name => new_name } }
+
+      it 'renames the organization' do
+        mock(renamed_organization).update!
+        subject
+      end
+
+      it 'displays the progress' do
+        mock_with_progress("Renaming to #{new_name}")
+        mock(renamed_organization).update!
+
+        subject
+      end
+
+      context 'and the name already exists' do
+        it 'fails' do
+          mock(renamed_organization).update! { raise CFoundry::OrganizationNameTaken }
+          expect { subject }.to raise_error(CFoundry::OrganizationNameTaken)
+        end
+      end
+    end
+  end
+end
