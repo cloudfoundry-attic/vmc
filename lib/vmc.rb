e7559fb3aabf498c4921023d3d92005ce6daceae
Alex Suraci and Tim Labeeuw <suraci.alex@gmail.com>
2012-12-05 09:52:20 -0800

    split up 'start' commands
    Change-Id: I595d47afa287bf645c56d874c9123af66491fcfb

--- lib/vmc.rb
+++ lib/vmc.rb
@@ -1,10 +1,9 @@
 require "vmc/version"
 
 require "vmc/cli"
-require "vmc/cli/start"
 require "vmc/cli/user"
 
-command_files = "../vmc/cli/{app,route,domain,organization,space,service}/*.rb"
+command_files = "../vmc/cli/{app,route,domain,organization,space,service,start}/*.rb"
 Dir[File.expand_path(command_files, __FILE__)].each do |file|
   require file unless File.basename(file) == 'base.rb'
 end
