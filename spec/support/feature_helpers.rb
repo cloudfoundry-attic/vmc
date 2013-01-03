f1c83abef9c32fd020dcd8f879d36f2261c20dd1
Elisabeth Hendrickson and Matthew Boedicker <mboedicker@rbcon.com>
2013-01-03 10:45:44 -0800

    More debugging statements to diagnose travis.
    Change-Id: Ia3bfbc49316bcbcb9fbaa8a5b6e5525a91b77091

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,11 +1,19 @@
 def vmc_ok(argv)
+  code = 0
+  output = ""
   with_output_to do |out|
     code = VMC::CLI.start(argv + ["--no-script"])
-    if code != 0
-      $stderr.puts out.string
-    end
     yield out.string.strip_progress_dots if block_given?
-    expect(code).to eq 0
+    output = "OUTPUT:\n#{out}\n\n"
+    # expect(code).to eq 0
+  end
+  if code != 0
+    if File.exist?("/home/travis/builds/cloudfoundry/vmc/spec/tmp/.vmc/crash")
+      $stderr.puts "Found the crash file"
+      crashes = File.readlines("/home/travis/builds/cloudfoundry/vmc/spec/tmp/.vmc/crash")
+      output = "#{output} CRASHLOGS:\n#{crashes}\n"
+    end
+    $stderr.puts output
   end
 end
 
