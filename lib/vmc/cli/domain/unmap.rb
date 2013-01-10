2c84ad124e32788178d521f05f618d3ec1d75c6f
Alex Suraci and Maria Shaldybina <suraci.alex@gmail.com>
2013-01-10 11:36:34 -0800

    merge 'remove/delete-domain' into 'unmap-domain'
    Change-Id: I4f5413c6fcdf5244c20c58339c752a8ee8c72bdd

---
+++ lib/vmc/cli/domain/unmap.rb
@@ -0,0 +1,56 @@
+require "vmc/cli/domain/base"
+
+module VMC::Domain
+  class Unmap < Base
+    desc "Unmap a domain from an organization or space"
+    group :domains
+    input :domain, :desc => "Domain to unmap", :argument => :required,
+          :from_given => by_name("domain")
+    input :organization, :desc => "Organization to unmap the domain from",
+          :aliases => %w{--org -o},
+          :default => proc { client.current_organization },
+          :from_given => by_name(:organization)
+    input :space, :desc => "Space to unmap the domain from",
+          :default => proc { client.current_space },
+          :from_given => by_name(:space)
+    input :delete, :desc => "Delete domain", :type => :boolean
+    input :really, :type => :boolean, :forget => true, :hidden => true,
+          :default => proc { force? || interact }
+    def unmap_domain
+      domain = input[:domain]
+
+      given_org = input.given?(:organization)
+      given_space = input.given?(:space)
+
+      org = input[:organization]
+      space = input[:space]
+
+      if input[:delete]
+        return unless input[:really, domain.name, :name]
+
+        with_progress("Deleting domain #{c(domain.name, :name)}") do
+          domain.delete!
+        end
+
+        return
+      end
+
+      given_space = true unless given_org || given_space
+
+      remove_domain(domain, space) if given_space
+      remove_domain(domain, org) if given_org
+    end
+
+    private
+
+    def remove_domain(domain, target)
+      with_progress("Unmapping #{c(domain.name, :name)} from #{c(target.name, :name)}") do
+        target.remove_domain(domain)
+      end
+    end
+
+    def ask_really(name, color)
+      ask("Really delete #{c(name, color)}?", :default => false)
+    end
+  end
+end
---
+++ spec/vmc/cli/domain/unmap_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe VMC::Domain::Unmap do
+  let(:global) { { :color => false } }
+  let(:given) { {} }
+  let(:client) { fake_client :current_organization => organization, :current_space => space }
+  let!(:cli) { described_class.new }
+
+  before do
+    stub(cli).client { client }
+    stub(cli).precondition { nil }
+    stub_output(cli)
+  end
+
+  let(:organization) { fake(:organization) }
+  let(:space) { fake(:space) }
+  let(:domain) { fake(:domain, :name => domain_name) }
+  let(:domain_name) { "some.domain.com" }
+
+  subject { invoke_cli(cli, :unmap_domain, inputs, given, global) }
+
+  context "when the --delete flag is given" do
+    let(:inputs) { { :domain => domain, :delete => true } }
+
+    it "asks for a confirmation" do
+      mock_ask("Really delete #{domain_name}?", :default => false) { false }
+      stub(domain).delete!
+      subject
+    end
+
+    context "and the user answers 'no' to the confirmation" do
+      it "does NOT delete the domain" do
+        stub_ask("Really delete #{domain_name}?", anything) { false }
+        dont_allow(domain).delete!
+        subject
+      end
+    end
+
+    context "and the user answers 'yes' to the confirmation" do
+      it "deletes the domain" do
+        stub_ask("Really delete #{domain_name}?", anything) { true }
+        mock(domain).delete!
+        subject
+      end
+    end
+  end
+
+  context "when a space is given" do
+    let(:inputs) { { :domain => domain, :space => space } }
+
+    it "unmaps the domain from the space" do
+      mock(space).remove_domain(domain)
+      subject
+    end
+  end
+
+  context "when an organization is given" do
+    let(:inputs) { { :domain => domain, :organization => organization } }
+
+    it "unmaps the domain from the organization" do
+      mock(organization).remove_domain(domain)
+      subject
+    end
+  end
+
+  context "when only the domain is given" do
+    let(:inputs) { { :domain => domain } }
+
+    it "unmaps the domain from the current space" do
+      mock(client.current_space).remove_domain(domain)
+      subject
+    end
+  end
+end
