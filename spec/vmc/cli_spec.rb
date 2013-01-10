950d0c8e8f1e808fc72ea2e96448f1b590b47097
Jennifer Hickey and Tim Labeeuw <jhickey@vmware.com>
2013-01-09 17:47:05 -0800

    Get the request and response traces into VMC crash file [finishes #41065481]
    Change-Id: I8339e6912e7a0d3ea78cb0eddf3b54e63b58bc44

--- spec/vmc/cli_spec.rb
+++ spec/vmc/cli_spec.rb
@@ -16,11 +16,11 @@ describe VMC::CLI do
     it 'wraps Timeout::Error with a more friendly message' do
       stub(cmd).precondition { raise CFoundry::Timeout.new(Net::HTTP::Get, "/foo") }
 
-      mock(cmd).err "GET /foo timed out"
+      mock(cmd).err 'GET /foo timed out'
       subject
     end
 
-    context "when the debug flag is on" do
+    context 'when the debug flag is on' do
       let(:inputs) { {:debug => true} }
 
       it 'reraises' do
@@ -29,13 +29,51 @@ describe VMC::CLI do
       end
     end
 
-    context "when the debug flag is off" do
+    context 'when the debug flag is off' do
       it 'outputs the crash log message' do
         stub(cmd).precondition { raise StandardError.new }
         mock(cmd).err /StandardError: StandardError\nFor more information, see .+\.vmc\/crash/
 
         expect { subject }.not_to raise_error(StandardError)
       end
+    end
+  end
+
+  describe '#log_error' do
+    subject do
+      cmd.log_error(exception)
+      File.read(VMC::CRASH_FILE)
+    end
+
+    context 'when the exception is a normal error' do
+      let(:exception) do
+        error = StandardError.new("gemfiles are kinda hard")
+        error.set_backtrace(["fo/gems/bar", "baz quick"])
+        error
+      end
+
+      it { should include "Time of crash:"}
+      it { should include "gemfiles are kinda hard" }
+      it { should include "bar" }
+      it { should_not include "fo/gems/bar" }
+      it { should include "baz quick" }
+    end
+
+    context 'when the exception is an APIError' do
+      let(:request) { Net::HTTP::Get.new("http://api.cloudfoundry.com/foo") }
+      let(:response) { Net::HTTPNotFound.new("foo", 404, "bar")}
+      let(:exception) do
+        error = CFoundry::APIError.new(request, response)
+        error.set_backtrace(["fo/gems/bar", "baz quick"])
+        error
+      end
+
+      before do
+        stub(response).body {"Response Body"}
+      end
+
+      it { should include "REQUEST: " }
+      it { should include "RESPONSE: " }
     end
   end
 end
