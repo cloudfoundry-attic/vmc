57032f0c4c4cde71290017af3488eaa06affd8d0
Alex Suraci and Maria Shaldybina <suraci.alex@gmail.com>
2013-01-10 11:13:03 -0800

    merge 'add/create-domain' into 'map-domain'
    Change-Id: Id680fa182b5082fe940c23be107788d9a3e0a5f9

---
+++ lib/vmc/cli/domain/map.rb
@@ -0,0 +1,52 @@
+require "vmc/cli/domain/base"
+
+module VMC::Domain
+  class Map < Base
+    desc "Map a domain to an organization or space"
+    group :domains
+    input :name, :desc => "Domain to map", :argument => :required
+    input :organization, :desc => "Organization to map the domain to",
+          :aliases => %w{--org -o},
+          :default => proc { client.current_organization },
+          :from_given => by_name(:organization)
+    input :space, :desc => "Space to map the domain to",
+          :default => proc { client.current_space },
+          :from_given => by_name(:space)
+    input :shared, :desc => "Create a shared domain", :default => false
+    def map_domain
+      domain = client.domain_by_name(input[:name])
+      given_org = input.given?(:organization)
+      given_space = input.given?(:space)
+      org = input[:organization]
+      space = input[:space]
+
+      given_space = true unless given_org || given_space
+
+      unless domain
+        domain = client.domain
+        domain.name = input[:name]
+        domain.owning_organization = org unless input[:shared]
+
+        with_progress("Creating domain #{c(domain.name, :name)}") do
+          domain.create!
+          org.add_domain(domain) if org && !given_org && !given_space
+        end
+      end
+
+      if given_space
+        add_domain(domain, space.organization)
+        add_domain(domain, space)
+      elsif given_org
+        add_domain(domain, org)
+      end
+    end
+
+    private
+
+    def add_domain(domain, target)
+      with_progress("Mapping #{c(domain.name, :name)} to #{c(target.name, :name)}") do
+        target.add_domain(domain)
+      end
+    end
+  end
+end
---
+++ spec/vmc/cli/domain/map_spec.rb
@@ -0,0 +1,141 @@
+require 'spec_helper'
+
+describe VMC::Domain::Map do
+  let(:global) { { :color => false } }
+  let(:given) { {} }
+  let(:client) { fake_client :current_organization => organization, :current_space => space }
+  let!(:cli) { described_class.new }
+
+  before do
+    stub(cli).client { client }
+    stub(cli).precondition { nil }
+    stub_output(cli)
+  end
+
+  let(:organization) { fake(:organization) }
+  let(:space) { fake(:space) }
+  let(:domain) { fake(:domain, :name => domain_name) }
+  let(:domain_name) { "some.domain.com" }
+
+  subject { invoke_cli(cli, :map_domain, inputs, given, global) }
+
+  shared_examples "binding a domain to a space" do
+    it "adds the domain to the space's organization" do
+      mock(space.organization).add_domain(domain)
+      stub(space).add_domain(domain)
+      subject
+    end
+
+    it 'adds the domain to the space' do
+      stub(space.organization).add_domain(domain)
+      mock(space).add_domain(domain)
+      subject
+    end
+  end
+
+  shared_examples "binding a domain to an organization" do
+    it 'does NOT add the domain to a space' do
+      any_instance_of(space.class) do |space|
+        dont_allow(space).add_domain(domain)
+      end
+    end
+
+    it 'adds the domain to the organization' do
+      mock(organization).add_domain(domain)
+      subject
+    end
+  end
+
+  shared_examples "mapping a domain to a space" do
+    context "when the domain does NOT exist" do
+      before do
+        stub(client).domain { domain }
+        stub(domain).create!
+        stub(space.organization).add_domain(domain)
+        stub(space).add_domain(domain)
+      end
+
+      it 'creates the domain' do
+        mock(domain).create!
+        subject
+        expect(domain.name).to eq domain_name
+        expect(domain.owning_organization).to eq organization
+      end
+
+      include_examples "binding a domain to a space"
+    end
+
+    context "when the domain already exists" do
+      let(:client) {
+        fake_client :domains => [domain],
+                    :current_organization => organization,
+                    :current_space => space
+      }
+
+      include_examples "binding a domain to a space"
+    end
+  end
+
+  context 'when a domain and a space are passed' do
+    let(:inputs) { { :space => space, :name => domain_name } }
+
+    include_examples "mapping a domain to a space"
+  end
+
+  context 'when a domain and an organization are passed' do
+    let(:inputs) { { :organization => organization, :name => domain_name } }
+
+    context "and the domain does NOT exist" do
+      before do
+        stub(client).domain { domain }
+        stub(domain).create!
+        stub(organization).add_domain(domain)
+      end
+
+      include_examples "binding a domain to an organization"
+
+      it 'adds the domain to the organization' do
+        mock(organization).add_domain(domain)
+        subject
+      end
+
+      context "and the --shared option is passed" do
+        let(:inputs) { { :organization => organization, :name => domain_name, :shared => true } }
+
+        it 'adds the domain to the organization' do
+          mock(domain).create!
+          subject
+          expect(domain.name).to eq domain_name
+        end
+
+        it "does not add the domain to a specific organization" do
+          stub(domain).create!
+          subject
+          expect(domain.owning_organization).to be_nil
+        end
+      end
+    end
+
+    context "and the domain already exists" do
+      let(:client) {
+        fake_client :domains => [domain],
+                    :current_organization => organization,
+                    :current_space => space
+      }
+
+      include_examples "binding a domain to an organization"
+    end
+  end
+
+  context 'when a domain, organization, and space is passed' do
+    let(:inputs) { { :name => domain_name, :organization => organization, :space => space } }
+
+    include_examples "mapping a domain to a space"
+  end
+
+  context 'when only a domain is passed' do
+    let(:inputs) { { :name => domain_name } }
+
+    include_examples "mapping a domain to a space"
+  end
+end
