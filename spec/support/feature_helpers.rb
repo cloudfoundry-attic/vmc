5caecfeafac9db2150c0b1c6ef3390959f6e4ace
Max Brunsfeld and Tim Labeeuw <framework-int+max+tim@rbcon.com>
2013-01-17 10:17:46 -0800

    Add debugging code to feature spec
    Change-Id: I1ce353d9ca7fde9c32e5126c714683c7ce76f9fe

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,12 +1,16 @@
 def vmc_ok(argv)
   Mothership.new.exit_status 0
   capture_output { VMC::CLI.start(argv + ["--no-script"]) }
+  output = stdout.string.strip_progress_dots
+  puts output if status != 0
   expect(status).to eq 0
-  yield stdout.string.strip_progress_dots if block_given?
+  yield output if block_given?
 end
 
 def vmc_fail(argv)
   capture_output { VMC::CLI.start(argv + ["--no-script"]) }
+  output = stdout.string.strip_progress_dots
+  puts output if status == 0
   expect(status).to eq 1
-  yield stdout.string.strip_progress_dots if block_given?
+  yield output if block_given?
 end
