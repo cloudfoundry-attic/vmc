519c2fe57351a4b60c5d5361b252120b2a24ab46
Alex Suraci & Chris Brown <pair+alex+cbrown@pivotallabs.com>
2013-02-04 16:17:29 -0800

    [finishes #43795625] install plugins with install script

--- script/install-stable-vmc.sh
+++ script/install-stable-vmc.sh
@@ -29,4 +29,16 @@ gem install cfoundry-*.gem
 popd
 rm -rf vmc-lib-tmp
 
+git clone git://github.com/cloudfoundry/vmc-plugins.git vmc-plugins-tmp
+pushd vmc-plugins-tmp
+for plugin in manifests console tunnel mcf admin; do
+  pushd $plugin
+  gem uninstall $plugin-vmc-plugin --all --ignore-dependencies --executables
+  gem build $plugin-vmc-plugin.gemspec
+  gem install $plugin-vmc-plugin-*.gem
+  popd
+done
+popd
+rm -rf vmc-plugins-tmp
+
 vmc -v
