efcf0fab0bfbae6426c6ff5b9f2892d117309d06
Dennis Schmidt & Ian Baker <pair+dschmidt+ibaker@pivotallabs.com>
2013-03-12 10:10:44 -0700

    make app names fuzzier in integration tests

--- spec/features/v2/push_flow_spec.rb
+++ spec/features/v2/push_flow_spec.rb
@@ -11,7 +11,7 @@ if ENV['VMC_V2_TEST_USER'] && ENV['VMC_V2_TEST_PASSWORD'] && ENV['VMC_V2_TEST_TA
     let(:password) { ENV['VMC_V2_TEST_PASSWORD'] }
 
     let(:app) do
-      fuzz = TRAVIS_BUILD_ID || Time.new.to_f.to_s.gsub(".", "_")
+      fuzz = TRAVIS_BUILD_ID.to_s + Time.new.to_f.to_s.gsub(".", "_")
       "hello-sinatra-#{fuzz}"
     end
 
